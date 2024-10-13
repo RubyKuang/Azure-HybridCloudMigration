67f86833c6e23c6d0b14a81c9cecdc35a07c93fc
RubyKuang <140364150+RubyKuang@users.noreply.github.com>
2024-10-13 20:02:04 +1300

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def productiontasks():
     return render_template('productiontasks.html', productiontasks=productiontasks)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
